bc2f2548da13306b2440bd2b8a6e50cd8e5e1575
Sam Pritchard <sampritchard@msn.com>
2018-03-04 13:50:24 +0000

    titles added

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -10,8 +10,8 @@ const BlogPage = () => (
     <h4><a href="blogs/gambling">Please gamble responsibly, but not too responsibly.</a></h4>
     <h4><a href="blogs/lessons-from-jobhunting">Lessons from job hunting.</a></h4>
     <h4><a href="blogs/moaning">Why do people moan ALL the time?</a></h4>
-    <h4><a href="blogs/new-year-resolutions"></a></h4>
-    <h4><a href="blogs/new-year-resolutions">Coming Soon.</a></h4>
+    <h4><a href="blogs/FAQs-diet">FAQs on diet.</a></h4>
+    <h4><a href="blogs/expensive-vegan">5 vegan meals.</a></h4>
     <h4><a href="blogs/new-year-resolutions">Coming Soon.</a></h4>
     <h4><a href="blogs/new-year-resolutions">Coming Soon.</a></h4>
   </div>
